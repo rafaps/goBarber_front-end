94bd5a1d543d8e650fd00a0841688b79f9bc8054
Rafael Paiva <r.rafapaiva@gmail.com>
2020-10-29 20:45:20 +1000

    Added SignUP page

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { FiLogIn, FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { Container, Content, Background } from './styles';
+
+import imgLogo from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: any): void {
+    console.log(data);
+  }
+
+  return (
+    <>
+      <Container>
+        <Background />
+
+        <Content>
+          <img src={imgLogo} alt="goBarber Logo" />
+          <Form onSubmit={handleSubmit}>
+            <h1>Sign Up</h1>
+
+            <Input name="email" icon={FiMail} placeholder="Email" />
+            <Input name="name" icon={FiUser} placeholder="Name" />
+
+            <Input
+              name="password"
+              icon={FiLock}
+              placeholder="Password"
+              type="password"
+            />
+
+            <Button type="submit" value="test">
+              Send
+            </Button>
+          </Form>
+
+          <a href="/">
+            <FiArrowLeft />
+            Back
+          </a>
+        </Content>
+      </Container>
+    </>
+  );
+};
+
+export default SignUp;
---
+++ src/pages/SignUp/styles.ts
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import signUpBackgroundImage from '../../assets/signup-img.png';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  align-content: center;
+
+  width: 100%;
+  max-width: 800px;
+
+  form {
+    margin: 80px;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+
+  > a {
+    color: #ffffff;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    &:hover {
+      color: ${shade(0.2, '#FFFFFF')};
+    }
+
+    svg {
+      margin-right: 15px;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signUpBackgroundImage}) no-repeat center;
+  background-size: cover;
+`;
